cb9b91acb4b002fc42c26cf481cea8fa7cdf414d
김의헌 <lot700@lotMac.local>
2026-03-29 14:01:16 +0900

    neuron_struct

--- 0nn.cpp
+++ 0nn.cpp
@@ -10,6 +10,34 @@ vector<arr3d> outputs;
 class Neuron;
 typedef vector<Neuron> Layer;
 
+struct Connection
+{
+    double weight;
+    double deltaWeight;
+};
+
+class Neuron
+{
+    public:
+        Neuron(unsigned numOutputs, unsigned myIndex);
+        void setOutputVal(double val) { m_outputVal = val; }
+        static double randomWeight(void) { return rand() / double(RAND_MAX); }
+    private:
+        double m_outputVal;
+        vector<Connection> m_outputWeights;
+        unsigned m_myIndex;
+};
+
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
+{
+    for (unsigned c = 0; c < numOutputs; ++c) {
+        m_outputWeights.push_back(Connection());
+        m_outputWeights.back().weight = randomWeight();
+    }
+
+    m_myIndex = myIndex;
+}
+
 class GetData
 {
     public:
@@ -17,7 +45,7 @@ class GetData
         void getTopology(vector<int> &topology, int &argc, char** argv);
 };
 void GetData::getTopology(vector<int> &topology, int &argc, char** argv) {
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
         topology.push_back(atoi(argv[i]));
         cout << topology[i - 1] << endl;
     }
@@ -34,10 +62,15 @@ class Net
 
 Net::Net(const vector<int> &topology) 
 {
-    int numlayers = topology.size();
+    int numlayers = (int)topology.size();
     for (int layNum = 0; layNum < numlayers; layNum++) {
         m_layer.push_back(Layer());
         int numOutputs = layNum == topology.size() - 1 ? 0 : topology[layNum + 1];
+        for (int neuronNum = 0; neuronNum <= topology[layNum]; ++neuronNum) {
+            m_layer.back().push_back(Neuron(numOutputs, neuronNum));
+            cout << "Made a Neuron!" << endl;
+        }
+        m_layer.back().back().setOutputVal(1.0);
     }
 }
 
